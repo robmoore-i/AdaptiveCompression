e72546d521c27f054acae5515135caad16e5a35a
Rob <robmoore121@gmail.com>
2017-12-17 21:34:21 +0000

    Cleaned up manual runner

--- Code/database_cracking/src/bm.rs
+++ Code/database_cracking/src/bm.rs
@@ -7,25 +7,15 @@ use std::iter;
 use rand::Rng;
 
 fn main() {
-    let n = 5;
+    let n = 100;
     let mut adjacency_list = randomly_connected_graph(n);
-    let selection = adjacency_list.select_in_two("src".to_string(), n+1);
-    let start_node;
-    match selection.get_col("src".to_string()) {
-        Some(ref src) => {
-            println!("src = {:?}", src.v);
-            start_node = *rand::thread_rng().choose(&src.v).unwrap();
-        },
-        None          => {
-            println!("src failed");
-            start_node = 3;
-        },
-    }
-    match selection.get_col("dst".to_string()) {
-        Some(ref dst) => println!("dst = {:?}", dst.v),
-        None          => println!("dst failed"),
-    }
-    bfs(&mut adjacency_list, start_node);
+    let all_nodes: Vec<i64> = (1..(n+1)).map(|x|x as i64).collect();
+    let start_node = *rand::thread_rng().choose(&all_nodes).unwrap();
+    let visited_order = bfs(&mut adjacency_list, start_node);
+    println!("Edge count: {}", adjacency_list.count);
+    println!("Visiting order: {:?}", visited_order);
+    println!("Cracker column: {:?}", adjacency_list.crk_col.crk);
+    println!("Base column: {:?}", adjacency_list.crk_col.v);
 }
 
 // Deals out the numbers from 0 to n-1 inclusive in a random order as usizes.
@@ -93,20 +83,19 @@ fn randomly_connected_graph(n: i64) -> Table {
             t.insert(&mut connections);
         }
     }
-    let src_nodes = t.get_col("src".to_string()).unwrap().v.clone();
     t.set_crk_col("src".to_string());
     let t_count = t.count;
     t.rearrange(deal(t_count).iter());
     t
 }
 
-// Given a 2-column ADJACENCY_LIST of src_node!dst_node, this function
-// visits every node in the graph from START_NODE.
-fn bfs(adjacency_list: &mut Table, start_node: i64) {
+// Given a 2-column ADJACENCY_LIST of src_node!dst_node, this function visits every node in the
+// graph from START_NODE.
+// Returns the nodes visited in the order in which they were visited.
+fn bfs(adjacency_list: &mut Table, start_node: i64) -> Vec<i64> {
     let mut frontier = vec![start_node];
     let mut visited = Vec::new();
     while !frontier.is_empty() {
-        println!("Visited {:?} ; Visiting {:?}", visited, frontier);
         // Add visited nodes
         visited.append(&mut frontier.clone());
         let prev_frontier = frontier.clone();
@@ -117,7 +106,6 @@ fn bfs(adjacency_list: &mut Table, start_node: i64) {
             match adjacency_list.cracker_select_in_three(src, src, true, true).get_col("dst".to_string()) {
                 Some(ref col) => for dst in col.v.clone() {
                     if !visited.contains(&dst) && !frontier.contains(&dst) {
-                        println!("Discovered {}", dst);
                         frontier.push(dst);
                     }
                 },
@@ -125,5 +113,5 @@ fn bfs(adjacency_list: &mut Table, start_node: i64) {
             }
         }
     }
-    println!("Visited {:?}", visited);
+    visited
 }
