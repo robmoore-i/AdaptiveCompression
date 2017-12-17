facf77acb414f4e5685fabbc92ae769e4f8a2db5
Rob <robmoore121@gmail.com>
2017-12-17 01:41:38 +0000

    Added more comments + implemented BFS (although BFS for a complete graph is not very interesting), however - found edge case. Needs fix

--- Code/database_cracking/src/bm.rs
+++ Code/database_cracking/src/bm.rs
@@ -7,20 +7,20 @@ use std::iter;
 
 fn main() {
     let n = 5;
-    let t = fully_connected_graph(n);
+    let mut t = fully_connected_graph(n);
     let selection = t.select_in_two("src".to_string(), n+1);
-    let src = selection.get_col("src".to_string());
-    let dst = selection.get_col("dst".to_string());
-    match src {
+    match selection.get_col("src".to_string()) {
         Some(ref col) => println!("src = {:?}", col.v),
         None          => println!("src failed"),
     }
-    match dst {
+    match selection.get_col("dst".to_string()) {
         Some(ref col) => println!("dst = {:?}", col.v),
         None          => println!("dst failed"),
     }
+    bfs(&mut t, 3);
 }
 
+// Deals out the numbers from 0 to n-1 inclusive in a random order as usizes.
 fn deal(n: usize) -> Vec<usize> {
     let mut dealing: Vec<i64> = Vec::with_capacity(n as usize);
     let half_n_as_f64 = (0.5*(n%2) as f64)+(n / 2) as f64;
@@ -39,6 +39,8 @@ fn deal(n: usize) -> Vec<usize> {
     dealing_usize
 }
 
+// A randomly shuffled adjacency list representing a complete graph for n nodes, columns are src, dst,
+// and src is the cracked column.
 fn fully_connected_graph(n: i64) -> Table {
     let mut t = Table::new();
     t.new_columns(vec!["src".to_string(), "dst".to_string()]);
@@ -60,5 +62,26 @@ fn fully_connected_graph(n: i64) -> Table {
 
 // Given a 2-column ADJACENCY_LIST of src_node!dst_node, this function
 // visits every node in the graph from START_NODE.
-fn bfs(adjacency_list: Table, start_node: i64) {
+fn bfs(adjacency_list: &mut Table, start_node: i64) {
+    let mut frontier = vec![start_node];
+    let mut visited = Vec::new();
+    while !frontier.is_empty() {
+        println!("Visited {:?}", frontier);
+        // Add visited nodes
+        visited.append(&mut frontier.clone());
+        let prev_frontier = frontier.clone();
+        frontier.clear();
+        // For each src in the previous frontier, find the dsts which haven't been visited yet,
+        // and add them to a new, empty frontier.
+        for src in prev_frontier {
+            match adjacency_list.cracker_select_in_three(src - 1, src + 1, false, false).get_col("dst".to_string()) {
+                Some(ref col) => for dst in col.v.clone() {
+                    if !visited.contains(&dst) {
+                        frontier.push(dst);
+                    }
+                },
+                None => panic!("bfs: No dst column in crack_in_three result for src node {}", src),
+            }
+        }
+    }
 }
