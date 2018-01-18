b70c2d71dc408583a91bd5d93d1964c36a718355
Rob <robmoore121@gmail.com>
2018-01-18 23:01:31 +0000

    Adding preclustering and unoptimised bfs profiling

--- Code/database_cracking/src/bm.rs
+++ Code/database_cracking/src/bm.rs
@@ -10,15 +10,21 @@ use time::PreciseTime;
 use time::SteadyTime;
 
 fn main() {
-    let n_readings = 5;
+    let n = 30000;
+    let mut adjacency_list = randomly_connected_tree(n);
+    let all_nodes: Vec<i64> = (1..(n + 1)).map(|x| x as i64).collect();
+    let start_node = *rand::thread_rng().choose(&all_nodes).unwrap();
+    preclustered_bfs(&mut adjacency_list, start_node);
+}
+
+fn build_graph_sizes(n_readings: i64, min_graph_size: i64, max_graph_size: i64, step: i64) -> Vec<i64> {
     let mut bm_graph_sizes: Vec<i64> = Vec::new();
-    for i in 0..n_readings {
-        for j in 20..30 {
-            bm_graph_sizes.push(200 * i + 1000 * j - 100);
-        }
+    let mut size = min_graph_size;
+    while size <= max_graph_size {
+        bm_graph_sizes.push(size);
+        size += step;
     }
-    bm_graph_sizes.push(30000);
-    benchmark_sparse_bfs_csv(bm_graph_sizes);
+    bm_graph_sizes
 }
 
 // Given a list of numbers, does a bfs benchmark for sparse graphs with a number of nodes given
@@ -36,7 +42,7 @@ fn benchmark_sparse_bfs_csv(ns: Vec<i64>) {
 // line of a csv file.
 fn benchmark_sparse_bfs(n: i64) {
     let adjacency_list = randomly_connected_tree(n);
-    let all_nodes: Vec<i64> = (1..(n+1)).map(|x|x as i64).collect();
+    let all_nodes: Vec<i64> = (1..(n + 1)).map(|x| x as i64).collect();
     let start_node = *rand::thread_rng().choose(&all_nodes).unwrap();
     print!("{},{},{}", n, adjacency_list.count, graph_density(n, adjacency_list.count));
     time_bfs(unoptimised_bfs,  &mut adjacency_list.clone(), start_node);
@@ -58,6 +64,30 @@ fn graph_density(n: i64, e: usize) -> f64 {
     (e as f64) / ((n * (n - 1)) as f64)
 }
 
+/* PROFILING
+    Functions and macros for profiling the performance of a function
+*/
+
+macro_rules! t_block {
+    ($work:block, $tvar:ident) => {
+            let start = PreciseTime::now();
+            $work;
+            let end = PreciseTime::now();
+            $tvar = $tvar + start.to(end);
+        };
+}
+
+macro_rules! t_expr {
+    ($work:expr, $tvar:ident) => {
+            let start = PreciseTime::now();
+            $work;
+            let end = PreciseTime::now();
+            $tvar = $tvar + start.to(end);
+        };
+}
+
+// Todo: Write a nice macro for printing all the profiler timing variables at the end of a fn.
+
 /* GRAPH BUILDING:
     Given a number of nodes N for a graph
 
@@ -67,7 +97,7 @@ fn graph_density(n: i64, e: usize) -> f64 {
 // Deals out the numbers from 0 to n-1 inclusive in a random order as usizes.
 fn deal(n: usize) -> Vec<usize> {
     let mut dealing: Vec<i64> = Vec::with_capacity(n as usize);
-    let half_n_as_f64 = (0.5*(n%2) as f64)+(n / 2) as f64;
+    let half_n_as_f64 = (0.5 * (n % 2) as f64) + (n / 2) as f64;
     let scaling_factor: f64 = half_n_as_f64 / (<i64>::max_value() as f64);
     while dealing.len() < n {
         let candidate: f64 = (half_n_as_f64 + scaling_factor * (rand::random::<i64>() as f64)).floor();
@@ -88,13 +118,13 @@ fn deal(n: usize) -> Vec<usize> {
 fn fully_connected_graph(n: i64) -> Table {
     let mut t = Table::new();
     t.new_columns(vec!["src".to_string(), "dst".to_string()]);
-    for i in 1..(n+1) {
+    for i in 1..(n + 1) {
         let mut connections = HashMap::new();
-        let i_vec: Vec<i64> = iter::repeat(i as i64).take((n-1) as usize).collect();
+        let i_vec: Vec<i64> = iter::repeat(i as i64).take((n - 1) as usize).collect();
         connections.insert("src".to_string(), i_vec);
-        let mut dst_nodes: Vec<i64> = Vec::with_capacity((n-1) as usize);
+        let mut dst_nodes: Vec<i64> = Vec::with_capacity((n - 1) as usize);
         dst_nodes.extend(1..i);
-        dst_nodes.extend((i+1)..(n+1));
+        dst_nodes.extend((i + 1)..(n + 1));
         connections.insert("dst".to_string(), dst_nodes);
         t.insert(&mut connections)
     }
@@ -108,7 +138,7 @@ fn fully_connected_graph(n: i64) -> Table {
 fn randomly_connected_tree(n: i64) -> Table {
     let mut t = Table::new();
     t.new_columns(vec!["src".to_string(), "dst".to_string()]);
-    let mut add_order: Vec<i64> = deal(n as usize).iter().map(|x|1 + *x as i64).collect();
+    let mut add_order: Vec<i64> = deal(n as usize).iter().map(|x| 1 + *x as i64).collect();
 
     let node_1 = *rand::thread_rng().choose(&add_order).unwrap();
     let mut node_2 = *rand::thread_rng().choose(&add_order).unwrap();
@@ -155,18 +185,9 @@ fn discover(dst: i64, visited: &mut Vec<i64>, frontier: &mut Vec<i64>) {
     }
 }
 
-macro_rules! t {
-    ($work:expr, $tvar:ident) => {
-            let start = PreciseTime::now();
-            $work;
-            let end = PreciseTime::now();
-            $tvar = $tvar + start.to(end);
-        };
-}
-
 fn adaptive_bfs(adjacency_list: &mut Table, start_node: i64) -> Vec<i64> {
     let mut frontier = vec![start_node];
-    let mut visited  = Vec::new();
+    let mut visited = Vec::new();
 
     while !frontier.is_empty() {
         // Add visited nodes
@@ -188,70 +209,117 @@ fn adaptive_bfs(adjacency_list: &mut Table, start_node: i64) -> Vec<i64> {
 }
 
 fn unoptimised_bfs(adjacency_list: &mut Table, start_node: i64) -> Vec<i64> {
-    let src_col = adjacency_list.get_col("src".to_string()).unwrap().v.clone();
-    let dst_col = adjacency_list.get_col("dst".to_string()).unwrap().v.clone();
-    let mut frontier = vec![start_node];
-    let mut visited  = Vec::new();
+    let precise_start_time = PreciseTime::now();
+    let start_time = SteadyTime::now();
+    let mut setup_time = start_time;
+    let mut frontier_cloning_time = start_time;
+    let mut searching_time = start_time;
+
+    let src_col;
+    let dst_col;
+    let mut frontier;
+    let mut visited;
+    t_block!({
+        src_col = adjacency_list.get_col("src".to_string()).unwrap().v.clone();
+        dst_col = adjacency_list.get_col("dst".to_string()).unwrap().v.clone();
+        frontier = vec![start_node];
+        visited = Vec::new();
+    }, setup_time);
+
     while !frontier.is_empty() {
-        visited.append(&mut frontier.clone());
-        let prev_frontier = frontier.clone();
-        frontier.clear();
+        let prev_frontier;
+        t_block!({
+            visited.append(&mut frontier.clone());
+            prev_frontier = frontier.clone();
+            frontier.clear();
+        }, frontier_cloning_time);
+
+        t_expr!(
         for src in prev_frontier {
             for i in 0..src_col.len() {
                 if src_col[i] == src {
                     discover(dst_col[i], &mut visited, &mut frontier);
                 }
             }
-        }
+        }, searching_time);
     }
+    println!("setup_time:            {}", setup_time - start_time);
+    println!("frontier_cloning_time: {}", frontier_cloning_time - start_time);
+    println!("searching_time:        {}", searching_time - start_time);
+    println!("total_time:            {}", precise_start_time.to(PreciseTime::now()));
     visited
 }
 
 fn preclustered_bfs(adjacency_list: &mut Table, start_node: i64) -> Vec<i64> {
-    let mut src_col = adjacency_list.get_col("src".to_string()).unwrap().v.clone();
-    let mut dst_col = adjacency_list.get_col("dst".to_string()).unwrap().v.clone();
-    let mut row_store: Vec<(i64, i64)> = Vec::with_capacity(adjacency_list.count);
-    for i in 0..adjacency_list.count {
-        row_store.push((src_col[i], dst_col[i]));
-    }
-    row_store.sort_by_key(|&k|k.0);
-    for i in 0..adjacency_list.count {
-        src_col[i] = row_store[i].0;
-        dst_col[i] = row_store[i].1;
-    }
+    let precise_start_time = PreciseTime::now();
+    let start_time = SteadyTime::now();
+    let mut setup_time = start_time;
+    let mut frontier_cloning_time = start_time;
+    let mut binary_search_time = start_time;
+    let mut span_out_search_time_loop_a = start_time;
+    let mut span_out_search_time_loop_b = start_time;
 
-    let mut frontier = vec![start_node];
-    let mut visited  = Vec::new();
+    let mut src_col;
+    let mut dst_col;
+    let mut row_store;
+    let mut frontier;
+    let mut visited;
+    t_block!({
+        src_col = adjacency_list.get_col("src".to_string()).unwrap().v.clone();
+        dst_col = adjacency_list.get_col("dst".to_string()).unwrap().v.clone();
+        row_store = Vec::with_capacity(adjacency_list.count);
+        for i in 0..adjacency_list.count {
+            row_store.push((src_col[i], dst_col[i]));
+        }
+        row_store.sort_by_key(|&k| k.0);
+        for i in 0..adjacency_list.count {
+            src_col[i] = row_store[i].0;
+            dst_col[i] = row_store[i].1;
+        }
+
+        frontier = vec![start_node];
+        visited = Vec::new();
+    }, setup_time);
+
     while !frontier.is_empty() {
-        visited.append(&mut frontier.clone());
-        let prev_frontier = frontier.clone();
-        frontier.clear();
+        let prev_frontier;
+        t_block!({
+            visited.append(&mut frontier.clone());
+            prev_frontier = frontier.clone();
+            frontier.clear();
+        }, frontier_cloning_time);
+
         for src in prev_frontier {
-            match src_col.binary_search(&src) {
-                Ok(i) => {
-                    let mut inc_idx = i.clone();
-                    let mut dec_idx = i.clone();
-                    loop {
-                        discover(dst_col[inc_idx], &mut visited, &mut frontier);
-                        inc_idx += 1;
-                        if inc_idx >= src_col.len() {
-                            break;
-                        } else if src_col[inc_idx] != src {
-                            break;
-                        }
+            let i;
+            t_expr!(i = src_col.binary_search(&src).unwrap(), binary_search_time);
+            let mut inc_idx = i.clone();
+            let mut dec_idx = i.clone();
+            t_expr!(
+                loop {
+                    discover(dst_col[inc_idx], &mut visited, &mut frontier);
+                    inc_idx += 1;
+                    if inc_idx >= src_col.len() {
+                        break;
+                    } else if src_col[inc_idx] != src {
+                        break;
                     }
-                    while src_col[dec_idx] == src {
-                        discover(dst_col[dec_idx], &mut visited, &mut frontier);
-                        if dec_idx == 0 {
-                            break;
-                        } else {
-                            dec_idx -= 1;
-                        }
+                }, span_out_search_time_loop_a);
+            t_expr!(
+                while src_col[dec_idx] == src {
+                    discover(dst_col[dec_idx], &mut visited, &mut frontier);
+                    if dec_idx == 0 {
+                        break;
+                    } else {
+                        dec_idx -= 1;
                     }
-                },
-                Err(_e) => panic!("preclustered_bfs: Binary search on a node that doesn't exist"),
-            }
+                }, span_out_search_time_loop_b);
         }
     }
+    println!("setup_time:                   {}", setup_time - start_time);
+    println!("frontier_cloning_time:        {}", frontier_cloning_time - start_time);
+    println!("binary_search_time:           {}", binary_search_time - start_time);
+    println!("span_out_search_time_loop_a:  {}", span_out_search_time_loop_a - start_time);
+    println!("span_out_search_time_loop_b:  {}", span_out_search_time_loop_b - start_time);
+    println!("total_time:                   {}", precise_start_time.to(PreciseTime::now()));
     visited
 }
